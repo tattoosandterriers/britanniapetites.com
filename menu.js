cd9ca5c776b57ebbfc04c693075eaaded3d80148
Kayleigh <kayleighbrubach@gmail.com>
2015-11-19 19:36:34 -0500

    first commit hanges to be committed:

---
+++ menu.js
@@ -0,0 +1,100 @@
+//----------DHTML Menu Created using AllWebMenus PRO ver 5.3-#898---------------
+//J:\Anne Mills_Backup\2012-09-05_18-00-01\Memeo\2012-09-05_18-00-01\C_\Users\Anne Mills\Documents\T&M\ABPRS\menu.awm
+awmRelativeCorner=5;
+var awmMenuName='menu';
+var awmLibraryBuild=898;
+var awmLibraryPath='/awmdata';
+var awmImagesPath='/awmdata/menu';
+var awmSupported=(navigator.appName + navigator.appVersion.substring(0,1)=="Netscape5" || document.all || document.layers || navigator.userAgent.indexOf('Opera')>-1 || navigator.userAgent.indexOf('Konqueror')>-1)?1:0;
+if (awmSupported){
+var nua=navigator.userAgent,scriptNo=(nua.indexOf('Chrome')>-1)?2:((nua.indexOf('Safari')>-1)?2:(nua.indexOf('Gecko')>-1)?2:((nua.indexOf('Opera')>-1)?4:1));
+var mpi=document.location,xt="";
+var mpa=mpi.protocol+"//"+mpi.host;
+var mpi=mpi.protocol+"//"+mpi.host+mpi.pathname;
+if(scriptNo==1){oBC=document.all.tags("BASE");if(oBC && oBC.length) if(oBC[0].href) mpi=oBC[0].href;}
+while (mpi.search(/\\/)>-1) mpi=mpi.replace("\\","/");
+mpi=mpi.substring(0,mpi.lastIndexOf("/")+1);
+var mpin=mpi;
+var e=document.getElementsByTagName("SCRIPT");
+for (var i=0;i<e.length;i++){if (e[i].src){if (e[i].src.indexOf(awmMenuName+".js")!=-1){xt=e[i].src.split("/");if (xt[xt.length-1]==awmMenuName+".js"){xt=e[i].src.substring(0,e[i].src.length-awmMenuName.length-3);if (e[i].src.indexOf("://")!=-1){mpi=xt;}else{if(xt.substring(0,1)=="/")mpi=mpa+xt; else mpi+=xt;}}}}}
+while (mpi.search(/\/\.\//)>-1) {mpi=mpi.replace("/./","/");}
+var awmMenuPath=mpi.substring(0,mpi.length-1);
+while (awmMenuPath.search("'")>-1) {awmMenuPath=awmMenuPath.replace("'","%27");}
+document.write("<SCRIPT SRC='"+(awmMenuPath+awmLibraryPath).replace(/\/$/,"")+"/awmlib"+scriptNo+".js'><\/SCRIPT>");
+var n=null;
+awmzindex=1000;
+}
+
+var awmImageName='spacer.jpg';
+var awmPosID='';
+var awmPosClass='';
+var awmSubmenusFrame='';
+var awmSubmenusFrameOffset;
+var awmOptimize=0;
+var awmHash='OPAOQXCNUNHAHAHAHAHAHAIAEAKAGS';
+var awmNoMenuPrint=1;
+var awmUseTrs=0;
+var awmSepr=["0","","",""];
+var awmMarg=[0,0,0,0];
+function awmBuildMenu(){
+if (awmSupported){
+awmCreateCSS(1,2,1,'#FFFFFF','#5B3F4E',n,'small-caps 18px OldCentury',n,'none','0','#000000','0px 0px 0px 0',0,'0px / 0px',n);
+awmCreateCSS(0,2,1,'#FFFFFF','#5B3F4E',n,'small-caps 18px OldCentury',n,'none','0','#000000','0px 0px 0px 0',0,'0px / 0px',n);
+awmCreateCSS(0,1,0,n,'#D7D6D4',n,n,n,'ridge','2','#5B3F4E','0px 0px 0px 0',0,'0px / 0px',n);
+awmCreateCSS(1,2,0,'#000000','#A5A4A5',n,'12px Georgia',n,'solid solid solid solid','2px 2px 2px 15','#C1C1C1 #888888 #888888 #C1C1C1','8px 20px 8px 20',1,'0px / 0px',n);
+awmCreateCSS(0,2,0,'#000000','#BABABA',n,'bold 12px Georgia',n,'solid solid solid solid','2px 2px 2px 8','#D5D5D5 #A0A0A0 #A0A0A0 #D5D5D5','8px 20px 8px 20',1,'0px / 0px',n);
+awmCreateCSS(0,2,0,'#000000','#BABABA',n,'italic bold 12px Georgia',n,'solid solid solid solid','2px 2px 2px 8','#D5D5D5 #A0A0A0 #A0A0A0 #D5D5D5','8px 20px 8px 20',1,'0px / 0px',n);
+awmCreateCSS(1,2,0,'#000000','#A5A4A5',n,'12px Georgia',n,'solid solid solid solid','2px 2px 2px 15','#C1C1C1 #888888 #888888 #C1C1C1','8px 20px 8px 20',0,'0px / 0px',n);
+awmCreateCSS(0,2,0,'#000000','#BABABA',n,'bold 12px Georgia',n,'solid solid solid solid','2px 2px 2px 8','#D5D5D5 #A0A0A0 #A0A0A0 #D5D5D5','8px 20px 8px 20',0,'0px / 0px',n);
+awmCreateCSS(0,2,0,'#000000','#BABABA',n,'italic bold 12px Georgia',n,'solid solid solid solid','2px 2px 2px 8','#D5D5D5 #A0A0A0 #A0A0A0 #D5D5D5','8px 20px 8px 20',0,'0px / 0px',n);
+awmCreateCSS(0,1,0,n,'#5B3F4E',n,n,n,'solid','2','#5B3F4E','0px 0px 0px 0',0,'0px / 0px',n);
+var s0=awmCreateMenu(0,0,0,0,1,0,0,0,5,10,10,0,0,2,1,1,1,n,n,100,2,0,0,0,200,-1,1,200,200,0,0,0,"0,0,0",n,n,n,n,n,n,n,n,1,0,0,0,0,0,0,0,1,0,0,1);
+it=s0.addItem(0,1,1,"Bred to Stand Tall",n,n,"","",n,n,n,n,n,0,0,0,0,0,n,n);
+it=s0.addItem(3,4,5,"ABPRS Home",n,n,"","index.htm",n,n,n,"index.htm",n,0,0,2,0,0,0,n);
+it=s0.addItem(6,7,8,"History",n,n,"","",n,n,n,n,n,0,0,2,0,0,1,n);
+var s1=it.addSubmenu(0,0,-1,1,1,0,0,9,1,1,0,n,n,100,-1,1,0,-1,1,200,200,0,0,"0,0,0",0,"0",1);
+it=s1.addItem(3,4,5,"History &amp; Photos",n,n,"","history.htm",n,n,n,"history.htm",n,0,0,2,0,0,2,n);
+it=s1.addItem(3,4,5,"Original Charter Breeders",n,n,"","charterbreeders.htm",n,n,n,"charterbreeders.htm",n,0,0,2,0,0,3,n);
+it=s0.addItem(6,7,8,"Club Information",n,n,"","",n,n,n,n,n,0,0,2,0,0,4,n);
+var s1=it.addSubmenu(0,0,-1,1,1,0,0,9,1,1,0,n,n,100,-1,2,0,-1,1,200,200,0,0,"0,0,0",0,"0",1);
+it=s1.addItem(3,4,5,"Constitution",n,n,"","constitution.htm",n,n,n,"constitution.htm",n,0,0,2,0,0,5,n);
+it=s1.addItem(3,4,5,"By-Laws",n,n,"","bylaws.htm",n,n,n,"bylaws.htm",n,0,0,2,0,0,6,n);
+it=s1.addItem(3,4,5,"Elected Officers",n,n,"","electedofficers.htm",n,n,n,"electedofficers.htm",n,0,0,2,0,0,36,n);
+it=s1.addItem(3,4,5,"Committee Members",n,n,"","commitmembers.htm",n,n,n,"commitmembers.htm",n,0,0,2,0,0,7,n);
+it=s1.addItem(3,4,5,"Club Logos",n,n,"","logos.htm",n,n,n,"logos.htm",n,0,0,2,0,0,37,n);
+it=s0.addItem(3,4,5,"ABPRS Sanction Request Form",n,n,"","SancReqForm.pdf",n,n,n,"SancReqForm.pdf","new",0,0,2,0,0,23,n);
+it=s0.addItem(3,4,5,"ABPRS Sanctioned Shows",n,n,"","sancshows.htm",n,n,n,"sancshows.htm",n,0,0,2,0,0,39,n);
+it=s0.addItem(6,7,8,"ABPRS Membership",n,n,"","",n,n,n,n,n,0,0,2,0,0,14,n);
+var s1=it.addSubmenu(0,0,-1,1,1,0,0,9,1,1,0,n,n,100,-1,4,0,-1,1,200,200,0,0,"0,0,0",0,"0",1);
+it=s1.addItem(3,4,5,"Membership Listing",n,n,"","memberlist.htm",n,n,n,"memberlist.htm",n,0,0,2,0,0,15,n);
+it=s1.addItem(3,4,5,"Membership Application",n,n,"","memberapplication.pdf",n,n,n,"memberapplication.pdf","new",0,0,2,0,0,16,n);
+it=s1.addItem(3,4,5,"Buy a Guidebook",n,n,"","guidebook.htm",n,n,n,"guidebook.htm",n,0,0,2,0,0,13,n);
+it=s1.addItem(3,4,5,"Hall of Fame",n,n,"","fame.htm",n,n,n,"fame.htm",n,0,0,2,0,0,17,n);
+it=s0.addItem(6,7,8,"National Shows",n,n,"","",n,n,n,n,n,0,0,2,0,0,8,n);
+var s1=it.addSubmenu(0,0,-1,1,1,0,0,9,1,1,0,n,n,100,-1,3,0,-1,1,200,200,0,0,"0,0,0",0,"0",1);
+it=s1.addItem(3,4,5,"ARBA Convention Wins &amp; Photos",n,n,"","convwins.htm",n,n,n,"convwins.htm",n,0,0,2,0,0,10,n);
+it=s1.addItem(3,4,5,"ABPRS National Wins &amp; Photos",n,n,"","natwins.htm",n,n,n,"natwins.htm",n,0,0,2,0,0,11,n);
+it=s1.addItem(3,4,5,"Awards Donation Form",n,n,"","AwardsDonForm.pdf",n,n,n,"AwardsDonForm.pdf","new",0,0,2,0,0,12,n);
+it=s0.addItem(3,4,5,"Specialty Clubs &amp; Photos",n,n,"","specialty.htm",n,n,n,"specialty.htm",n,0,0,2,0,0,9,n);
+it=s0.addItem(6,7,8,"Sweepstake Results",n,n,"","",n,n,n,n,n,0,0,2,0,0,18,n);
+var s1=it.addSubmenu(0,0,-1,1,1,0,0,9,1,1,0,n,n,100,-1,6,0,-1,1,200,200,0,0,"0,0,0",0,"0",1);
+it=s1.addItem(3,4,5,"Current",n,n,"","sweeps.htm",n,n,n,"sweeps.htm",n,0,0,2,0,0,24,n);
+it=s1.addItem(3,4,5,"Past",n,n,"","sweepspast.htm",n,n,n,"sweepspast.htm",n,0,0,2,0,0,25,n);
+it=s0.addItem(6,7,8,"Show Wins",n,n,"","",n,n,n,n,n,0,0,2,0,0,19,n);
+var s1=it.addSubmenu(0,0,-1,1,1,0,0,9,1,1,0,n,n,100,-1,5,0,-1,1,200,200,0,0,"0,0,0",0,"0",1);
+it=s1.addItem(3,4,5,"Best In Show",n,n,"","bis.htm",n,n,n,"bis.htm",n,0,0,2,0,0,20,n);
+it=s1.addItem(3,4,5,"Reserve In Show",n,n,"","ris.htm",n,n,n,"ris.htm",n,0,0,2,0,0,21,n);
+it=s1.addItem(3,4,5,"Best 4-Class",n,n,"","4class.htm",n,n,n,"4class.htm",n,0,0,2,0,0,22,n);
+it=s0.addItem(3,4,5,"ABPRS Newsletter",n,n,"","news.htm",n,n,n,"news.htm",n,0,0,2,0,0,26,n);
+it=s0.addItem(6,7,8,"All About The Breed",n,n,"","",n,n,n,n,n,0,0,2,0,0,28,n);
+var s1=it.addSubmenu(0,0,-1,1,1,0,0,9,1,1,0,n,n,100,-1,7,0,-1,1,200,200,0,0,"0,0,0",0,"0",1);
+it=s1.addItem(3,4,5,"Recognized Varieties",n,n,"","varietiesrec.htm",n,n,n,"varietiesrec.htm",n,0,0,2,0,0,29,n);
+it=s1.addItem(3,4,5,"Unrecognized Varieties",n,n,"","varietiesunrec.htm",n,n,n,"varietiesunrec.htm",n,0,0,2,0,0,30,n);
+it=s1.addItem(3,4,5,"Judging the Britannia Petite",n,n,"","judging.htm",n,n,n,"judging.htm",n,0,0,2,0,0,31,n);
+it=s1.addItem(3,4,5,"Britannia Petite Body Type",n,n,"","type.htm",n,n,n,"type.htm",n,0,0,2,0,0,32,n);
+it=s1.addItem(3,4,5,"Posing the Britannia Petite",n,n,"","posing.htm",n,n,n,"posing.htm",n,0,0,2,0,0,33,n);
+it=s1.addItem(3,4,5,"Certificate of Development Holders",n,n,"","COD.htm",n,n,n,"COD.htm",n,0,0,2,0,0,34,n);
+it=s0.addItem(3,4,5,"ABPRS Store",n,n,"","merchandise.htm",n,n,n,"merchandise.htm",n,0,0,2,0,0,27,n);
+it=s0.addItem(3,4,5,"Links",n,n,"","links.htm",n,n,n,"links.htm",n,0,0,2,0,0,35,n);
+s0.pm.buildMenu();
+}}
